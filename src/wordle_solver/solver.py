377316bf8237a00abf834e625728c446222b3cb6
Seto Balian <seto.balian@gmail.com>
2023-05-07 11:51:36 +0100

    Add load_words

--- src/wordle_solver/solver.py
+++ src/wordle_solver/solver.py
@@ -3,12 +3,20 @@ import pathlib
 import pkg_resources
 
 
-class Solver:
-    def __init__(self) -> None:
-        # Read possible guesses (from https://gist.github.com/cfreshman)
-        words_path = pathlib.Path(
+def load_words() -> list[str]:
+    """Read possible guesses (source: https://gist.github.com/cfreshman)."""
+
+    return (
+        pathlib.Path(
             pkg_resources.resource_filename(
                 "wordle_solver", "wordle-nyt-words-14855.txt"
             )
         )
-        self.words = words_path.read_text().split()
+        .read_text()
+        .split()
+    )
+
+
+class Solver:
+    def __init__(self) -> None:
+        self.words = set(load_words())
--- tests/test_wordle_solver.py
+++ tests/test_wordle_solver.py
@@ -1,5 +1,9 @@
-def test_words(solver_class):
-    words = solver_class.words
+from wordle_solver import solver
+
+
+def test_load_words():
+    words = solver.load_words()
     assert all(len(word) == 5 for word in words)
     assert len(set(words)) == len(words)
     assert all(word.islower() for word in words)
+    assert len(words) == 14855
