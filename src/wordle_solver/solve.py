18c278a7ba16be1db25b6ebcf7c7b3f1b767a614
Seto Balian <seto.balian@gmail.com>
2023-05-07 15:42:02 +0100

    Use importlib instead of pkg_resources

--- src/wordle_solver/solve.py
+++ src/wordle_solver/solve.py
@@ -1,12 +1,10 @@
 import collections
 import concurrent.futures
 import functools
-import pathlib
 import random
 import string
+from importlib import resources
 from typing import TypedDict
-
-import pkg_resources
 
 ALPHABET = set(string.ascii_lowercase)
 
@@ -14,15 +12,9 @@ ALPHABET = set(string.ascii_lowercase)
 def load_words() -> list[str]:
     """Read possible guesses (source: https://gist.github.com/cfreshman)."""
 
-    return (
-        pathlib.Path(
-            pkg_resources.resource_filename(
-                "wordle_solver", "wordle-nyt-words-14855.txt"
-            )
-        )
-        .read_text()
-        .split()
-    )
+    return resources.read_text(
+        "wordle_solver", "wordle-nyt-words-14855.txt"
+    ).splitlines()
 
 
 class Lookups(TypedDict):
