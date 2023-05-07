9bc254fd30363ccd68ab2a83d86851e69e9cd00e
Seto Balian <seto.balian@gmail.com>
2023-05-07 12:11:27 +0100

    Add lookups

--- src/wordle_solver/solver.py
+++ src/wordle_solver/solver.py
@@ -1,6 +1,11 @@
+import collections
 import pathlib
+import string
+from typing import TypedDict
 
 import pkg_resources
+
+ALPHABET = set(string.ascii_lowercase)
 
 
 def load_words() -> list[str]:
@@ -17,6 +22,45 @@ def load_words() -> list[str]:
     )
 
 
+class Lookups(TypedDict):
+    contains_at: dict[tuple[str, int], set[str]]
+    does_not_contain: dict[str, set[str]]
+    contains_not_at: dict[tuple[str, int], set[str]]
+
+
 class Solver:
     def __init__(self) -> None:
         self.words = set(load_words())
+        self.lookups = self.build_lookups()
+
+    def build_lookups(self) -> Lookups:
+        """Iterate over the words and populate lookups."""
+
+        contains: dict[str, set[str]] = collections.defaultdict(lambda: set())
+        does_not_contain: dict[str, set[str]] = collections.defaultdict(
+            lambda: set()
+        )
+        contains_at: dict[tuple[str, int], set[str]] = collections.defaultdict(
+            lambda: set()
+        )
+
+        for word in self.words:
+            for position, letter in enumerate(word):
+                contains[letter].add(word)
+                contains_at[(letter, position)].add(word)
+            for letter in ALPHABET - set(word):
+                does_not_contain[letter].add(word)
+
+        contains_not_at: dict[tuple[str, int], set[str]] = {}
+
+        for letter in string.ascii_lowercase:
+            for position in range(5):
+                contains_not_at[(letter, position)] = (
+                    contains[letter] - contains_at[(letter, position)]
+                )
+
+        return {
+            "contains_at": dict(contains_at),
+            "does_not_contain": dict(does_not_contain),
+            "contains_not_at": dict(contains_not_at),
+        }
--- tests/test_wordle_solver.py
+++ tests/test_wordle_solver.py
@@ -7,3 +7,35 @@ def test_load_words():
     assert len(set(words)) == len(words)
     assert all(word.islower() for word in words)
     assert len(words) == 14855
+
+
+def test_solver_words(solver_class):
+    assert solver_class.words == set(solver.load_words())
+
+
+def test_lookups(solver_class):
+    for lookup in solver_class.lookups.values():
+        assert set.union(*(lookup.values())) == solver_class.words
+
+    assert "apple" in solver_class.lookups["contains_at"][("a", 0)]
+    assert "apple" not in solver_class.lookups["contains_at"][("b", 1)]
+
+    assert "zebra" in solver_class.lookups["does_not_contain"]["q"]
+    assert "zebra" not in solver_class.lookups["does_not_contain"]["z"]
+
+    assert "apple" in solver_class.lookups["contains_not_at"][("a", 1)]
+    assert "apple" not in solver_class.lookups["contains_not_at"][("a", 0)]
+    assert "green" not in solver_class.lookups["contains_not_at"][("a", 0)]
+
+    assert (
+        set(letter for letter, _ in solver_class.lookups["contains_at"])
+        == solver.ALPHABET
+    )
+    assert (
+        set(letter for letter in solver_class.lookups["does_not_contain"])
+        == solver.ALPHABET
+    )
+    assert (
+        set(letter for letter, _ in solver_class.lookups["contains_not_at"])
+        == solver.ALPHABET
+    )
